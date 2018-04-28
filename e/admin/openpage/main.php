a2ae8aed5da7701a8dcfb1f6ff19fd6ba43ac0a0
mengmanzbh <1361006944@qq.com>
2018-04-28 14:34:03 +0800

    OK

---
+++ e/admin/openpage/main.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<title>说明</title>
+</head>
+<body>
+请选择左边的管理菜单
+</body>
+</html>
---
+++ e/class/EmpireCMS_version.php
@@ -0,0 +1,11 @@
+<?php
+
+define('EmpireCMS_VERSION','7.5');
+
+define('EmpireCMS_CHARVER','GBK');
+
+define('EmpireCMS_LASTTIME','201804091030');
+
+define('EmpireCMS_UPDATE','1');
+
+?>
